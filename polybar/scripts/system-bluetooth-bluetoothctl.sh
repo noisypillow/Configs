f0e087cfc207be873d3040cf8c41e6ac29b369aa
noisypillow <noisypillow@protonmail.com>
2022-07-23 20:54:19 +0200

    added starship config and modified polybar config

--- polybar/scripts/system-bluetooth-bluetoothctl.sh
+++ polybar/scripts/system-bluetooth-bluetoothctl.sh
@@ -3,7 +3,7 @@
 bluetooth_print() {
     bluetoothctl | while read -r; do
         if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
-            printf ''
+            #printf ''
 
             devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
             counter=0
@@ -12,6 +12,9 @@ bluetooth_print() {
                 device_info=$(bluetoothctl info "$device")
 
                 if echo "$device_info" | grep -q "Connected: yes"; then
+                    if [ $counter == 0 ]; then
+                        printf ''
+                    fi
                     device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
 
                     if [ $counter -gt 0 ]; then
